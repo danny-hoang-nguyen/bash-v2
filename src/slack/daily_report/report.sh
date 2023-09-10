652266a68f6f5fcc4c7bab5428b300f0f5ca8414
danny <dung.nguyen@leap.expert>
2023-09-10 14:46:17 +0700

    change to use input params for convenience

--- src/slack/daily_report/report.sh
+++ src/slack/daily_report/report.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
 base_path="/Users/dannynguyen/Desktop/bash/src/slack/daily_report"
 input="$base_path/info"
 template="$base_path/template.json"
 
+while getopts u:c:t: flag; do
+  case "${flag}" in
+  u) user=${OPTARG} ;;
+  c) channel=${OPTARG} ;;
+  t) token=${OPTARG} ;;
+  *) echo "UNKNOWN ARG" ;;
+  esac
+done
+echo "value of -u: $user"
+echo "value of -c: $channel"
+echo "value of -t: $token"
+
 while read -r id stage summary; do
   echo "$id, $stage, $summary"
-  payload=$(cat $template | sed "s/ID/$id/g" | sed "s/report-block-id/$(date +%s000)/g" | sed "s/SUMMARY/$summary/g")
+  payload=$(cat $template | sed "s/ID/$id/g" | sed "s/report-block-id/$(date +%s000)/g" | sed "s/SUMMARY/$summary/g" | sed "s/REPORT_CHANNEL/$channel/g" | sed "s/USERID_SLACK/$user/g")
   case $stage in
 
   0)
@@ -40,7 +53,7 @@ echo "request payload: $payload"
 
 res=$(curl --location -s 'https://slack.com/api/chat.postMessage' \
   --header 'Content-type: application/json' \
-  --header "Authorization: Bearer $SLACK_BOT_TOKEN" \
+  --header "Authorization: Bearer $token" \
   --data-raw "$payload" | json_pp)
 echo "==========================="
 echo "request response: $res"
